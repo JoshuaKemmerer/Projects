50fd1340490b8ca2e05e5f802ef6c7659a5fc752
JoshuaKemmerer <joshkemm@gmail.com>
2015-05-15 18:35:36 -0400

    Adding the new Plan Of Study project
    Adding the new 'Plan Of Study' project as a completely new, unedited
    project in ASP.NET MVC

---
+++ Plasty (Plan of Study)/Plasty/Plasty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Plasty.MvcApplication" Language="C#" %>
---
+++ Plasty (Plan of Study)/Plasty/Plasty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plasty.Startup))]
+namespace Plasty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
